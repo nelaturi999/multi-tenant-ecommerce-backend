aa55022ee3845851302e0b15f6d9056c6774d7ea
nelaturi999 <sasankarureddy@gmail.com>
2026-09-17 13:35:28 +0530

    Fix Vercel CORS origin

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
-  "https://multi-tenant-frontend-lac.vercel.app/"];
+  "https://multi-tenant-frontend-lac.vercel.app"];
 
 
 // =====================================================
